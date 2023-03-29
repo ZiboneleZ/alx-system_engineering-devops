5c0003c79ea88953707ebf8397f7c99421acf64a
ZiboneleZ <zibonelez47@gmail.com>
2023-03-29 12:57:03 -0700

    New commit

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,10 @@
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+package { 'python3-pip':
+  ensure => 'installed',
 }
+
+exec { 'install-flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  unless  => '/usr/bin/pip3 freeze | /bin/grep Flask | /bin/grep -q 2.1.0',
+  require => Package['python3-pip'],
+}
+
